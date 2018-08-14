3b4d83359380419d61a3ce1a2d2edc050d807c02
Raydson Ferreira <raydson.ferreira.lemaf@gmail.com>
2018-08-14 10:33:33 -0300

    subindo treinamento em andamento

---
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -0,0 +1,45 @@
+<template>
+    <button @click="disparaAcao()" class="botao botao-perigo">{{ rotulo }}</button>
+</template>
+
+<script>
+export default {
+    props: {
+        tipo: {
+            required: true,
+            type: String
+        },
+
+        rotulo: {
+            required: true,
+            type: String
+        }
+    },
+
+    methods:{
+        disparaAcao(){
+            if(confirm('Confirma Operação?'))
+                this.$emit('botaoAtivado');
+        }
+    }
+}
+</script>
+
+<style>
+.botao {
+    display: inline-block;
+    padding: 10px;
+    border-radius: 3px;
+    margin: 10px;
+    font-size: 1.2em;
+}
+
+.botao-perigo {background: firebrick;
+    color: white;
+}
+
+.botao-padrao {
+    background: darkcyan;
+    color: white;
+}
+</style>
